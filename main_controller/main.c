ae8656c310c5b7da78bc02e462f4bd1322540ad4
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-23 19:00:05 +0200

    Update example program for main controller
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main_controller/main.c
+++ main_controller/main.c
@@ -2,10 +2,22 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "spi.h"
 
 int main(){
+	spi_init();
+
+	int screen_counter = 0;
+
 	while(1){
-		_delay_ms(100);
+		spi_send(screen_counter);
+
+		screen_counter++;
+		if(screen_counter > 25){
+			screen_counter = 0;
+		}
+
+		_delay_ms(10);
 	}
 
 	return 0;
