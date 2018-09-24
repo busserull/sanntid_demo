2e1de28601a3af9dd75ea1f4a60a2ac41f9c39ce
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-24 19:34:28 +0200

    Update main controller example code
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main_controller/main.c
+++ main_controller/main.c
@@ -2,11 +2,25 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
 #include "spi.h"
 #include "led_driver.h"
 #include "uart.h"
 
-static char led_out_array[27];
+#define STX 0x02
+#define ETX 0x03
+
+static uint8_t led_status[25] = {0};
+
+void broadcast_led_status(uint8_t * leds){
+	uart_send(STX);
+
+	for(int i = 0; i < 25; i++){
+		uart_send(leds[i]);
+	}
+
+	uart_send(ETX);
+}
 
 int main(){
 	spi_init();
@@ -18,7 +32,13 @@ int main(){
 	int screen_counter = 0;
 	int setting = 1;
 
+	int segment_left;
+	int segment_right;
+
 	while(1){
+		led_array_get(led_status);
+		broadcast_led_status(led_status);
+
 		spi_send(screen_counter);
 
 		if(screen_counter > 25){
@@ -26,9 +46,10 @@ int main(){
 			setting = !setting;
 		}
 
-		uart_send(uart_receive());
+		segment_left = screen_counter / 5;
+		segment_right = screen_counter % 5;
+		led_array_set(segment_left, segment_right, setting);
 
-		led_array_set(screen_counter / 5, screen_counter % 5, setting);
 		led_grid_write();
 
 		screen_counter++;
