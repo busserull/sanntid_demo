01c0c9886f7ace4531ee96527bc49d0df09b14c8
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-24 21:36:53 +0200

    Minor code touch-up
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main_controller/led_driver.h
+++ main_controller/led_driver.h
@@ -6,7 +6,7 @@ void led_array_init();
 
 void led_array_zero();
 
-void led_array_get(uint8_t *array);
+void led_array_get(uint8_t * array);
 
 void led_array_set(uint8_t x, uint8_t y, uint8_t value);
 
