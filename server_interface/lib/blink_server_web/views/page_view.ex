654a73d16638cc921190e6c016c727fb2771e8c1
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-24 17:00:35 +0200

    Add basic web page layout
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- server_interface/lib/blink_server_web/views/page_view.ex
+++ server_interface/lib/blink_server_web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule BlinkWeb.PageView do
   use BlinkWeb, :view
+
+  def format_led_number(led_row, led_col) do
+    "#{(led_row - 1) * 5 + led_col}"
+    |> String.pad_leading(2, "0")
+  end
+
 end
