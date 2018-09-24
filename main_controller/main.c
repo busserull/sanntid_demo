22c6726c8b301f870a2d5593a86e794f9a40d88b
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-24 12:11:51 +0200

    Update main controller example code
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main_controller/main.c
+++ main_controller/main.c
@@ -4,26 +4,35 @@
 #include <util/delay.h>
 #include "spi.h"
 #include "led_driver.h"
+#include "uart.h"
 
 int main(){
 	spi_init();
+	uart_init();
+
+	while(1){
+		uart_send('a');
+		_delay_ms(500);
+	}
 
 	led_array_init();
 	led_array_zero();
 
 	int screen_counter = 0;
+	int setting = 1;
 
 	while(1){
 		spi_send(screen_counter);
 
-		screen_counter++;
 		if(screen_counter > 25){
 			screen_counter = 0;
+			setting = !setting;
 		}
 
-		led_array_set(screen_counter / 5, screen_counter % 5, 1);
+		led_array_set(screen_counter / 5, screen_counter % 5, setting);
 		led_grid_write();
 
+		screen_counter++;
 		_delay_ms(100);
 	}
 
