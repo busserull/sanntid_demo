3058fb2993b2920f9cb96bab00f5b173a9504bdd
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2018-09-25 12:48:41 +0200

    Change led_array_set to led_array_toggle
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main_controller/led_driver.c
+++ main_controller/led_driver.c
@@ -57,9 +57,9 @@ void led_array_get(uint8_t *array){
 	}
 }
 
-void led_array_set(uint8_t x, uint8_t y, uint8_t value){
+void led_array_toggle(uint8_t x, uint8_t y){
 	if(x < 5 && x >= 0 && y < 5 && y >= 0){
-		led_array[x][y] = 1 && value;
+		led_array[x][y] = !led_array[x][y];
 	}
 	led_grid_write();
 }
--- main_controller/led_driver.h
+++ main_controller/led_driver.h
@@ -8,7 +8,7 @@ void led_array_zero();
 
 void led_array_get(uint8_t * array);
 
-void led_array_set(uint8_t x, uint8_t y, uint8_t value);
+void led_array_toggle(uint8_t x, uint8_t y);
 
 void led_grid_write();
 
